965c5bc8b52c1cbd20496dfc73b0b9fef21deddd
Peter G. Hancock <hancock@spamcop.net>
2019-09-18 17:11:29 +0100

    How in hell does git work?

--- make-pdf.sh
+++ make-pdf.sh
@@ -1,4 +1,7 @@
-lhs2TeX --poly arithmetic-code.lhs > arithmetic-code.tex ; rubber --pdf arithmetic-code 
-(cd ~/work/arithmetic ;	lhs2Tex --math arithmetic.lhs > arithmetic.tex ; rubber --pdf arithmetic) ; cp ~/work/arithmetic/arithmetic.pdf ~/src/amen-calculator/ 
+lhs2TeX --poly arithmetic-code.lhs > arithmetic-code.tex 
+rubber --pdf arithmetic-code 
+(cd ~/work/arithmetic ;	make)   # lhs2Tex --math arithmetic.lhs > arithmetic.tex ; rubber --pdf arithmetic) 
+(cd ~/work/arithmetic/ ; make publish)  #arithmetic.pdf ~/src/amen-calculator/ 
+(cd ~/work/www ; make)
 
 
